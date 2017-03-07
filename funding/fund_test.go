7d6b7b5d07872cc5f839e0951a2082fcc3e577d3
Pierre <pierre.gaxatte@gmail.com>
2017-03-07 07:46:02 +0100

    Finished tutorial but not functional

--- funding/fund_test.go
+++ funding/fund_test.go
@@ -1,8 +1,70 @@
 package funding
 
-import "testing"
+import (
+    "sync"
+    "testing"
+)
 
-func BenchmarkFund(b *testing.B) {
-    // Adds as much funds as we have iterations
+const WORKERS = 10
+
+func BenchmarkWithdrawals(b *testing.B) {
+    // Skip N = 1
+    if b.N < WORKERS {
+        return
+    }
+
+    /*
+    // Add as many dollars as we have iterations this run
     fund := NewFund(b.N)
+
+    => Use server implementation
+    */
+    server := NewFundServer(b.N)
+
+    // Casually assume b.N divides cleanly
+    dollarsPerFounder := b.N / WORKERS
+
+    // WaitGroup structs don't need to be initialized
+    // (their "zero value" is ready to use).
+    // So, we just declare one and then use it.
+    var wg sync.WaitGroup
+
+    for i := 0; i < WORKERS; i++ {
+        // Let the waitgroup know we're adding a goroutine
+        wg.Add(1)
+
+        // Spawn off a founder worker, as a closure
+        go func() {
+            // Mark this worker done when the function finishes
+            defer wg.Done()
+
+            for i := 0; i < dollarsPerFounder; i++ {
+                /*
+                fund.Withdraw(1)
+                => Use server implementation
+                */
+                server.Commands <- WithdrawCommand{ Amount: 1 }
+            }
+
+        }() // Remember to call the closure!
+    }
+
+    /*
+    // Wait for all the workers to finish
+    wg.Wait()
+
+    if fund.Balance() != 0 {
+        b.Error("Balance wasn't zero:", fund.Balance())
+    }
+    => Use reponse channel from server
+    */
+
+    balanceResponseChan := make(chan int)
+    server.Commands <- BalanceCommand{ Response: balanceResponseChan }
+    balance := <- balanceResponseChan
+
+    if balance != 0 {
+        b.Error("Balance wasn't zero:", balance)
+    }
 }
+
